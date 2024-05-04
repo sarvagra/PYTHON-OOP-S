3b386a1991240a86a3f550f8ba35a46c97da2597
sarvagra <sarvagras977@gmail.com>
2024-05-04 19:18:58 +0530

    added file

---
+++ 06_abstraction.py
@@ -0,0 +1,29 @@
+# importing abc class for abstraction
+
+import abc
+
+class abst():
+
+    @abc.abstractmethod
+    def one(self):
+        return "this is the first function"
+    
+    @abc.abstractmethod
+    def two(self):
+        return "this is the second function"
+    
+    @abc.abstractmethod 
+    def three(self):
+        return "this is the third function"
+    
+class abst2(abst):
+    def one(self):
+        return "this will store numbers"
+    
+    def two(self):
+        return "this will store words"
+    
+def three(self):
+    return "this will store sentences"
+
+
